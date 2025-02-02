4effd733dd5003bfaaeeba2c59241697cb7f0d10
arebirdsreal <astronil123@gmail.com>
2025-02-02 13:18:11 +0530

    intial commit

---
+++ highest_score.py
@@ -0,0 +1,14 @@
+student_scores = [4,1,7,8,4,10,3,9,6]
+
+# METHOD 1
+# temp = student_scores[0]
+# for score in student_scores:
+#     if score > temp:
+#         temp = score
+# print(temp)
+
+
+# METHOD 2
+# print(max(student_scores))
+
+
---
+++ project_password_generator.py
@@ -0,0 +1,10 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+number = [1,2,3,4,5,6,7,8,9,0]
+symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', ',', '.', '/', '\\', '{', '}',
+        '[', ']', '|', '`', '~', '<', '>', '?']
+
+number_of_letters = int(input("How many letters do you want in your password? "))
+number_of_numbers = int(input("How many numbers do you want in your passwodd? "))
+number_of_symbols = int(input("how many symbols do you want in your password? "))
+

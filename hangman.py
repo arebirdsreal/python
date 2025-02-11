8c9faebe58b7fb39c909719af4c7de76e67b7bac
arebirdsreal <astronil123@gmail.com>
2025-02-12 00:59:41 +0530

    c

--- hangman.py
+++ hangman.py
@@ -3,22 +3,32 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-guess = input("Guess letter: ")
-guess.lower()
-
-for letter in chosen_word:
-    if guess == letter:
-        print("Right")
-    else:
-        print("Wrong")
-
 placeholder = ""
+
 word_length = len(chosen_word)
 for i in range(0, word_length):
     placeholder += "_"
 print(placeholder)
 
+number_of_guesses = 0
+while number_of_guesses < 7
+
+
+guess = input("Guess letter: ")
+guess.lower()
+
+
 display = ""
+for letter in chosen_word:
+    if guess == letter:
+        # print("Right")
+        display += letter
+    else:
+        # print("Wrong")
+        display += "_"
+
+print(display)
+
 
 
 

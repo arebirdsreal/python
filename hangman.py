222fdf496b80f03a5dc841a5451abdd68086e585
arebirdsreal <astronil123@gmail.com>
2025-02-12 00:32:42 +0530

    a

--- hangman.py
+++ hangman.py
@@ -1,3 +1,14 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
-print(random.choice(word_list))
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+guess = input("Guess letter: ")
+guess.lower()
+
+for letter in chosen_word:
+    if guess == letter:
+        print("Right")
+    else:
+        print("Wrong")
+

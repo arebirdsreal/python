58a54b8bb67ff2e25932b12dbfb06b84a264cb36
arebirdsreal <114248672+arebirdsreal@users.noreply.github.com>
2025-03-16 15:24:35 +0530

    Update blind auction.py

--- blind auction.py
+++ blind auction.py
@@ -1,15 +1,15 @@
-bids = {}
+auction_info = {}
 choice = ""
 key = ''
 choice = input("Enter choice: ")
 while choice == 'yes':
     key = input("Enter your name: ")
-    bids[key] = int(input("Enter your bid: "))
+    auction_info[key] = int(input("Enter your bid: "))
     if choice == 'yes':
         choice = input("Enter choice: ")
 highest_bid = 0
-for bidder in bids:
-    if bids[bidder] > highest_bid:
-        highest_bid = bids[bidder]
+for bidder in auction_info:
+    if auction_info[bidder] > highest_bid:
+        highest_bid = auction_info[bidder]
         winner = bidder
 print(f"The highest bid is {highest_bid} by {winner}!")

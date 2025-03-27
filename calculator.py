e47ab2451163557e31a9e8048a21cc288c6e069e
arebirdsreal <astronil123@gmail.com>
2025-03-28 02:00:59 +0530

    incomplete calculator

--- calculator.py
+++ calculator.py
@@ -1,4 +1,47 @@
-def calc(operation, operand_1, operand_2):
+import calculator_art as art
+print(art.art)
+def calc(operator, operand_1, operand_2):
+    if operator == '+':
+        return operand_1 + operand_2
+    elif operator == '-':
+        return operand_1 - operand_2
+    elif operator == '*':
+        return operand_1 * operand_2
+    elif operator == '/':
+        return operand_1 / operand_2
+choice = ''
+choice = input("+ for addition \n- for subtraction \n* for multiplication \n"
+                "/ for division: \n")
+a,b = input("Enter the 2 operands seperated by comma: \n").split(',')
+operand_1, operand_2 = int(a), int(b)
+
+want_to_continue = ''
+while want_to_continue != 'n':
+    result = calc(operator=choice, operand_1=operand_1, operand_2=operand_2)
+    print(f"{result}\n\n")
+    want_to_continue = input("Do you want to continue? Enter 'y' or 'n' only.")
+    if want_to_continue == 'y' or want_to_continue == 'n':
+            None
+    else:
+        print("Strictly enter 'y' or 'n' only")
+
+
+    while want_to_continue == 'y':
+        choice = input("+ for addition \n, - for subtraction \n, * for multiplication \n, "
+                "/ for division: \n")
+        operand_2 = int(input("Enter second operand: "))
+        result = calc(operator=choice, operand_1=result, operand_2=operand_2)
+        print(result)
+        want_to_continue = input("Do you want to continue? ")
+        if want_to_continue == 'y' or want_to_continue == 'n':
+            None
+        else:
+            print("Strictly enter 'y' or 'n' only")
+        
+
+
+
+
   
   
   

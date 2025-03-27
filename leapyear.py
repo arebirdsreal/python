08bc76da8727f9b46985092c122c4ada13aadbb8
arebirdsreal <astronil123@gmail.com>
2025-03-27 13:59:01 +0530

    final leap year

--- leapyear.py
+++ leapyear.py
@@ -1,4 +1,5 @@
 def is_leap_year(year):
+    """takes a year as input and returns true if its a leap year"""
     if year % 4 != 0 or year % 100 == 0 and year % 400 != 0:
         return False
     else:

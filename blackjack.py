dc09d4ebc92109c9fb1be42c85b0818276ede018
arebirdsreal <astronil123@gmail.com>
2025-03-28 21:01:18 +0530

    update blackjack

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,6 @@
-import math
+import random
 def deal_card():
+  """Returns a random card value"""
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-  return 
+  return random.choice(cards)  #works
+
